246da26ab515014c56408d80e682e8f5643702d3
TomTom830 <thomasmartins83@gmail.com>
2019-02-27 21:48:49 +0100

    todo2 fait

---
+++ todo2/todo2.js
@@ -0,0 +1,39 @@
+var todoApp = angular.module('todoApp', []);
+todoApp.controller('TodoCtrl',['$scope', function($scope){
+
+    //init array of task
+    $scope.taskSet = [
+        {
+            taskName: 'faire le ménage',
+            done : false
+        },{
+            taskName: 'Ranger le courrier',
+            done: true
+        },{
+            taskName: 'Pronmener le chien',
+            done: false
+        }];
+
+    //method to add task
+    $scope.addTask = function () {
+        
+        if($scope.taskName !== ''){
+            //add task to array
+            $scope.taskSet.push({
+                taskName: $scope.taskName,
+                done: false
+            });
+
+            //clear taskName value
+            $scope.taskName = '';
+        }
+    };
+
+    $scope.delTask = function () {
+        for(var idx=0; idx < $scope.taskSet.length; idx++){
+            if($scope.taskSet[idx].taskName.localeCompare($scope.taskName2)==0){
+                $scope.taskSet.splice(idx,1);
+            }
+        }
+    }
+}]);
